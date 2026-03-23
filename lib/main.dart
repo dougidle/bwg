fa0b3d4f1f0c42d59a22645e31626fd499978b48
dougidle <137324266+dougidle@users.noreply.github.com>
2026-03-23 14:00:52 +0000

    Small change

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const BWGApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BWGApp extends StatelessWidget {
+  const BWGApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Barming Wargamers',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,9 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: Colors.green),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Barming Wargames Community'),
     );
   }
 }
